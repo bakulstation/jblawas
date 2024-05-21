451cf6ea6802457e2270a0544dc51d7f5d049897
bakulstation <praptosoe025@gmail.com>
2024-05-21 17:37:53 +0700

    Update payloads.php

--- PPPwn/payloads.php
+++ PPPwn/payloads.php
@@ -23,7 +23,7 @@ if (isset($_POST['back'])){
 
 print("<html> 
 <head>
-<title>PI-Pwn</title>
+<title>Sukses-Jailbreak</title>
 <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
 <style>
 
