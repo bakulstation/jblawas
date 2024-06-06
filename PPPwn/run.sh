7cd3b510e3a4c5859c29d125eb85584e644dadb2
bakulstation <praptosoe025@gmail.com>
2024-06-06 16:44:44 +0700

    Update run.sh

--- PPPwn/run.sh
+++ PPPwn/run.sh
@@ -4,39 +4,15 @@ if [ ! -f /boot/firmware/PPPwn/config.sh ]; then
 INTERFACE="eth0" 
 FIRMWAREVERSION="11.00" 
 SHUTDOWN=true
-USBETHERNET=false
 PPPOECONN=false
-VMUSB=false
-DTLINK=false
+DTLINK=true
 else
 source /boot/firmware/PPPwn/config.sh
 fi
 echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/unbind 
 echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/bind
 PITYP=$(tr -d '\0' </proc/device-tree/model) 
-if [[ $PITYP == *"Raspberry Pi 2"* ]] ;then
-coproc read -t 15 && wait "$!" || true
-CPPBIN="pppwn7"
-VMUSB=false
-elif [[ $PITYP == *"Raspberry Pi 3"* ]] ;then
-coproc read -t 10 && wait "$!" || true
-CPPBIN="pppwn64"
-VMUSB=false
-elif [[ $PITYP == *"Raspberry Pi 4"* ]] ;then
-coproc read -t 5 && wait "$!" || true
-CPPBIN="pppwn64"
-elif [[ $PITYP == *"Raspberry Pi 5"* ]] ;then
-coproc read -t 5 && wait "$!" || true
-CPPBIN="pppwn64"
-elif [[ $PITYP == *"Raspberry Pi Zero 2"* ]] ;then
-coproc read -t 8 && wait "$!" || true
-CPPBIN="pppwn64"
-VMUSB=false
-elif [[ $PITYP == *"Raspberry Pi Zero"* ]] ;then
-coproc read -t 10 && wait "$!" || true
-CPPBIN="pppwn11"
-VMUSB=false
-elif [[ $PITYP == *"Raspberry Pi"* ]] ;then
+if [[ $PITYP == *"Raspberry Pi"* ]] ;then
 coproc read -t 15 && wait "$!" || true
 CPPBIN="pppwn11"
 VMUSB=false
@@ -57,52 +33,32 @@ echo -e "\n\n\033[36m _____  _____  _____
 |_|    |_|    |_|      \\_/\\_/ |_| |_|\033[0m
 \n\033[33mhttps://github.com/TheOfficialFloW/PPPwn\033[0m\n" | sudo tee /dev/tty1
 sudo systemctl stop pppoe
-if [ $USBETHERNET = true ] ; then
-	echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/unbind
-	coproc read -t 2 && wait "$!" || true
-	echo '1-1' | sudo tee /sys/bus/usb/drivers/usb/bind
-	coproc read -t 5 && wait "$!" || true
-	sudo ip link set $INTERFACE up
-   else	
-	sudo ip link set $INTERFACE down
-	coproc read -t 5 && wait "$!" || true
-	sudo ip link set $INTERFACE up
-fi
-echo -e "\n\033[36m$PITYP\033[92m\nFirmware:\033[93m $FIRMWAREVERSION\033[92m\nInterface:\033[93m $INTERFACE\033[0m" | sudo tee /dev/tty1
-echo -e "\033[92mPPPwn:\033[93m C++ $CPPBIN \033[0m" | sudo tee /dev/tty1
-if [ $VMUSB = true ] ; then
-   UDEV=$(sudo blkid | grep '^/dev/sd' | cut -f1 -d':')
-   if [[ -z $UDEV ]] ;then
-      UDEV="/media/PPPwn/pwndev"
-	  echo -e "\033[92mVirtual Drive:\033[93m Enabled\033[0m" | sudo tee /dev/tty1
-	else
-	  echo -e "\033[92mFlash Drive:\033[93m Enabled\033[0m" | sudo tee /dev/tty1
-   fi
-   sudo modprobe g_mass_storage file=$UDEV stall=0 ro=0 removable=1
-fi
+
+echo -e "\n\033[36m$PITYP\033[92m\nFirmware:\033[93m $FIRMWAREVERSION\033[92m\nKoneksi:\033[93m $INTERFACE\033[0m" | sudo tee /dev/tty1
+echo -e "\033[92mPS4 Hen PPPwn:\033[93m C++ Spa++ Mokondo Edition $CPPBIN \033[0m" | sudo tee /dev/tty1
 if [ $PPPOECONN = true ] ; then
-   echo -e "\033[92mInternet Access:\033[93m Enabled\033[0m" | sudo tee /dev/tty1
+   echo -e "\033[92mKoneksi Internet:\033[93m Dinyalakan\033[0m" | sudo tee /dev/tty1
 else   
-   echo -e "\033[92mInternet Access:\033[93m Disabled\033[0m" | sudo tee /dev/tty1
+   echo -e "\033[92mKoneksi Internet:\033[93m Dimatikan\033[0m" | sudo tee /dev/tty1
 fi
-if [[ ! $(ethtool $INTERFACE) == *"Link detected: yes"* ]]; then
-   echo -e "\033[31mWaiting for link\033[0m" | sudo tee /dev/tty1
-   while [[ ! $(ethtool $INTERFACE) == *"Link detected: yes"* ]]
+if [[ ! $(ethtool $INTERFACE) == *"Link Detected: yes"* ]]; then
+   echo -e "\033[31mMenunggu Sambungan\033[0m" | sudo tee /dev/tty1
+   while [[ ! $(ethtool $INTERFACE) == *"Link Detected: yes"* ]]
    do
       coproc read -t 2 && wait "$!" || true
    done
-   echo -e "\033[32mLink found\033[0m\n" | sudo tee /dev/tty1
+   echo -e "\033[32mSambungan Ditemukan\033[0m\n" | sudo tee /dev/tty1
 fi
 PIIP=$(hostname -I) || true
 if [ "$PIIP" ]; then
    echo -e "\n\033[92mIP: \033[93m $PIIP\033[0m" | sudo tee /dev/tty1
 fi
-echo -e "\n\033[95mReady for console connection\033[0m\n" | sudo tee /dev/tty1
+echo -e "\n\033[95mSiap untuk koneksi konsol\033[0m\n" | sudo tee /dev/tty1
 while [ true ]
 do
 ret=$(sudo /boot/firmware/PPPwn/$CPPBIN --interface "$INTERFACE" --fw "${FIRMWAREVERSION//.}" --stage1 "/boot/firmware/PPPwn/stage1_$FIRMWAREVERSION.bin" --stage2 "/boot/firmware/PPPwn/stage2_$FIRMWAREVERSION.bin")
 if [ $ret -ge 1 ] ; then
-        echo -e "\033[32m\nConsole PPPwned! \033[0m\n" | sudo tee /dev/tty1
+        echo -e "\033[32m\nKonsol Berhasil Terjailbreak PPPwn! Hepi Gaming \033[0m\n" | sudo tee /dev/tty1
 		if [ $PPPOECONN = true ] ; then
 			sudo systemctl start pppoe
 			if [ $DTLINK = true ] ; then
@@ -122,6 +78,6 @@ if [ $ret -ge 1 ] ; then
 		fi
         exit 0
    else
-        echo -e "\033[31m\nFailed retrying...\033[0m\n" | sudo tee /dev/tty1
+        echo -e "\033[31m\nGagal mencoba ulang sampai mencret...\033[0m\n" | sudo tee /dev/tty1
 fi
 done
